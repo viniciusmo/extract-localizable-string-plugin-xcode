1d22f6e3d454815790df266838f8a1004427ed1c
viniciusmo <viniciusmo@viniciusmos-MacBook-Pro.local>
2014-07-01 10:26:34 -0300

    Changing view to popover

---
+++ ExtractLocalization/ExtractLocationPopoverView.h
@@ -0,0 +1,13 @@
+//
+//  ExtractLocationPopoverView.h
+//  ExtractLocalization
+//
+//  Created by viniciusmo on 7/1/14.
+//  Copyright (c) 2014 nanaimostudio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ExtractLocationPopoverView : NSView
+
+@end
---
+++ ExtractLocalization/ExtractLocationPopoverView.m
@@ -0,0 +1,29 @@
+//
+//  ExtractLocationPopoverView.m
+//  ExtractLocalization
+//
+//  Created by viniciusmo on 7/1/14.
+//  Copyright (c) 2014 nanaimostudio. All rights reserved.
+//
+
+#import "ExtractLocationPopoverView.h"
+
+@implementation ExtractLocationPopoverView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
